d0573d4fa7489d726a2038a40a0d83690ca22283
E-zu <edergutierrez636@gmail.com>
2025-04-01 08:11:16 -0600

    add.practica(conjuntos).py

---
+++ practica(conjuntos).py
@@ -0,0 +1,22 @@
+#c={1,2,3}
+#c.add(4)
+#c.remove(1)    #Error si no se encuentra 
+#c.discard(1)   #No hay error si no se encuentra
+#print(1 in c)
+#print(c)
+
+
+#Crear un conjunto de elementos 
+# agregatr un valor que no existe 
+# eliminar un valor que no existe 
+# verificar si existe un calor 
+# agregar un valor repetido
+
+
+c={1,5,9,6,3}
+c.add(2)
+c.add(5) 
+c.remove(3)
+c.discard(4)
+print( 5 in c)
+print (c)
